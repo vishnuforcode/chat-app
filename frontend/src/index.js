466638672978d0b18eb18cadcc0a904a8909cec1
vishnuforcode <vishnuforgrow@gmail.com>
2024-09-24 23:39:41 +0530

    added favicon

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -8,17 +8,20 @@ import { Provider } from "react-redux";
 import store from "./redux/Store";
 import { PersistGate } from 'redux-persist/integration/react'
 import persistStore from "redux-persist/es/persistStore";
+import { BrowserRouter } from "react-router-dom";
 
 let persistor = persistStore(store)
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
+    <BrowserRouter>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
       <App />
       <Toaster />
       </PersistGate>
     </Provider>
+    </BrowserRouter>
   </React.StrictMode> 
 );
